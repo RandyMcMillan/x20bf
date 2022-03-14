c3357be34f4248757c0e57d1584ad2e175d0eeb7
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-03-14 17:45:29 -0400

    tests/test_time_functions.py: lower case imports

--- tests/test_time_functions.py
+++ tests/test_time_functions.py
@@ -4,21 +4,21 @@ import getopt as G
 import sys as S
 
 from time_functions import (
-    BTC_UNIX_TIME_SECONDS,
-    NETWORK_MODULUS,
-    NETWORK_WEEBLE,
-    NETWORK_WEEBLE_WOBBLE,
-    NETWORK_WOBBLE,
-    UNIX_TIME_SECONDS,
     blockcypher_height,
     btc_time,
     btc_unix_time_millis,
+    btc_unix_time_seconds,
     get_millis,
     get_seconds,
     mempool_height,
     move_block_time,
+    network_modulus,
+    network_weeble,
+    network_weeble_wobble,
+    network_wobble,
     touch_time,
     unix_time_millis,
+    unix_time_seconds,
 )
 
 loop = asyncio.new_event_loop()
@@ -30,13 +30,13 @@ print(get_seconds())
 print(blockcypher_height())
 print(btc_time())
 print(btc_unix_time_millis())
-print(BTC_UNIX_TIME_SECONDS())
+print(btc_unix_time_seconds())
 print(unix_time_millis())
-print(UNIX_TIME_SECONDS())
-print(NETWORK_MODULUS())
-print(NETWORK_WEEBLE_WOBBLE())
-print(NETWORK_WEEBLE())
-print(NETWORK_WOBBLE())
+print(unix_time_seconds())
+print(network_modulus())
+print(network_weeble_wobble())
+print(network_weeble())
+print(network_wobble())
 # print(mempool_height())
 loop.run_until_complete(mempool_height())
 
