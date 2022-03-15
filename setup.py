6d8f85b7f3c347f1690e4ab0a83e3b614a586f0b
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-03-15 17:36:46 -0400

    Squashed 'x20bf/depends/git/git/ext/gitdb/gitdb/ext/smmap/' content from commit db8810096
    git-subtree-dir: x20bf/depends/git/git/ext/gitdb/gitdb/ext/smmap
    git-subtree-split: db8810096503dd8a1f5a021ff39be907417f90a7

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import smmap
+
+if os.path.exists("README.md"):
+    long_description = open('README.md', encoding="utf-8").read().replace('\r\n', '\n')
+else:
+    long_description = "See https://github.com/gitpython-developers/smmap"
+
+setup(
+    name="smmap",
+    version=smmap.__version__,
+    description="A pure Python implementation of a sliding window memory map manager",
+    author=smmap.__author__,
+    author_email=smmap.__contact__,
+    url=smmap.__homepage__,
+    platforms=["any"],
+    license="BSD",
+    packages=find_packages(),
+    zip_safe=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
